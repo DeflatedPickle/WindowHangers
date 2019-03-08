2fefd57d62230c014d07f0ac691e5de98a7ee34d
DeflatedPickle <DeflatedPickle@gmail.com>
2019-03-08 22:12:29 +0000

    Added physics

--- src/main/kotlin/com/deflatedpickle/hangerchan/Hangerchan.kt
+++ src/main/kotlin/com/deflatedpickle/hangerchan/Hangerchan.kt
@@ -1,15 +1,33 @@
 package com.deflatedpickle.hangerchan
 
 import com.deflatedpickle.windowhangers.WindowUtil
+import com.sun.jna.Native
+import com.sun.jna.platform.win32.User32
+import com.sun.jna.platform.win32.WinDef
+import com.sun.jna.platform.win32.WinUser
+import org.dyn4j.collision.Fixture
+import org.dyn4j.dynamics.Body
+import org.dyn4j.dynamics.BodyFixture
+import org.dyn4j.dynamics.World
+import org.dyn4j.geometry.Geometry
+import org.dyn4j.geometry.MassType
+import org.dyn4j.geometry.Shape
+import org.dyn4j.samples.framework.Graphics2DRenderer
 import java.awt.*
 import java.awt.event.ActionListener
+import java.awt.geom.AffineTransform
 import java.util.concurrent.ThreadLocalRandom
+import javax.swing.JFrame
 import javax.swing.JPanel
+import javax.swing.SwingUtilities
 import javax.swing.Timer
 
-class Hangerchan : JPanel() {
+class Hangerchan(val myFrame: JFrame, val world: World) : JPanel() {
     val sheet = SpriteSheet("/hangerchan/Hangerchan", 8, 10)
     var currentAction = Action.Idle
+
+    val body = Body()
+    val windowBody = Body()
 
     // -1 = Left, 1 = Right
     var direction = 1.0
@@ -19,8 +37,20 @@ class Hangerchan : JPanel() {
 
     var currentFrame = 0
 
+    var monitor: WinUser.HMONITOR? = null
+    val monitorInfo = WinUser.MONITORINFO()
+
+    val windowShapes = mutableListOf<BodyFixture>()
+
     init {
         isOpaque = false
+
+        val shape = Geometry.createRectangle(sheet.spriteWidth / 2.0, sheet.spriteHeight / 2.0)
+        body.addFixture(shape)
+        world.addBody(body)
+
+        windowBody.gravityScale = 0.0
+        world.addBody(windowBody)
 
         val timer = Timer(120, ActionListener {
             currentFrame++
@@ -73,6 +103,19 @@ class Hangerchan : JPanel() {
                 }
             }
 
+            for (i in WindowUtil.getAllWindowRects()) {
+                windowShapes.add(BodyFixture(Geometry.createRectangle(i.right - i.left.toDouble(), i.bottom - i.top.toDouble())))
+
+                if (windowBody.fixtures != windowShapes) {
+                    windowBody.removeAllFixtures()
+
+                    for (s in windowShapes) {
+                        windowBody.addFixture(s)
+                    }
+                }
+            }
+
+            world.step(1)
             this.repaint()
         })
         timer.start()
@@ -83,13 +126,23 @@ class Hangerchan : JPanel() {
         val g2D = g as Graphics2D
         g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
 
-        g.drawImage(sheet.spriteMap[currentAction.toString()]!![currentFrame], 0, 0, sheet.spriteWidth / 2, sheet.spriteHeight / 2, this)
+        if (monitor == null) {
+            monitor = User32.INSTANCE.MonitorFromWindow(WinDef.HWND(Native.getComponentPointer(myFrame)), User32.MONITOR_DEFAULTTONEAREST)
+            User32.INSTANCE.GetMonitorInfo(monitor, monitorInfo)
+        }
+
+        println(body.transform)
+        g.drawImage(sheet.spriteMap[currentAction.toString()]!![currentFrame], (body.transform.translationX * 10).toInt(), -(body.transform.translationY * 10).toInt(), sheet.spriteWidth / 2, sheet.spriteHeight / 2, this)
 
         // Debug prints
+        // TODO: Window intersections, stop cluttering up the draw space
         g2D.color = Color.RED
         g2D.stroke = BasicStroke(4f)
         for (i in WindowUtil.getAllWindowRects()) {
             g2D.drawRect(i.left, i.top, i.right - i.left, i.bottom - i.top)
         }
+
+        g2D.color = Color.GREEN
+        g2D.drawRect(monitorInfo.rcWork.left, monitorInfo.rcWork.top, monitorInfo.rcWork.right - monitorInfo.rcWork.left, monitorInfo.rcWork.bottom - monitorInfo.rcWork.top)
     }
 }
--- src/main/kotlin/com/deflatedpickle/hangerchan/main.kt
+++ src/main/kotlin/com/deflatedpickle/hangerchan/main.kt
@@ -1,36 +1,27 @@
 package com.deflatedpickle.hangerchan
 
-import com.deflatedpickle.windowhangers.HookPoint
-import com.deflatedpickle.windowhangers.WindowHanger
-import com.deflatedpickle.windowhangers.WindowUtil
-import com.sun.jna.Native
-import com.sun.jna.platform.win32.User32
-import com.sun.jna.platform.win32.WinDef
-import com.sun.jna.platform.win32.WinUser
+import org.dyn4j.dynamics.World
+import org.dyn4j.geometry.Vector2
 import java.awt.Color
-import java.awt.Dimension
-import java.awt.Point
-import java.awt.event.ActionListener
-import java.util.concurrent.ThreadLocalRandom
 import javax.swing.JFrame
-import javax.swing.JLabel
-import javax.swing.Timer
 
 
 @Suppress("KDocMissingDocumentation")
 fun main(args: Array<String>) {
     val frame = JFrame("Hanger-chan")
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
 
     frame.isUndecorated = true
     frame.isAlwaysOnTop = true
     frame.background = Color(0, 0, 0, 0)
 
-    // frame.minimumSize = Dimension(220 / 2, 256 / 2)
-    // frame.setSize(220 / 2, 256 / 2)
-
     frame.extendedState = JFrame.MAXIMIZED_BOTH
 
-    frame.contentPane.add(Hangerchan())
+    val world = World()
+    world.gravity = Vector2(0.0, -300.0)
+
+    val hangerchan = Hangerchan(frame, world)
+    frame.contentPane.add(hangerchan)
 
     frame.pack()
     frame.isVisible = true
